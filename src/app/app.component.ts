8e7d1817c71065b8075413f3a1e7d2299db08b6c
Dylan Lewis <dyldlewis@gmail.com>
2017-08-18 12:57:09 -0700

    create animal-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,12 @@ import { Animal } from './animal.model';
 })
 export class AppComponent {
   title = 'app works!';
-  masterAnmialList: Animal[] = [
+  masterAnimalList: Animal[] = [
     new Animal("Roger", "Monkey", 3, "Carnivore", "Magic Tree House", 3, "Male", "Teryaiki Sauce", "Humans"),
     new Animal("Katie", "Tiger", 5, "Carnivore", "Rain Forest Exibit", 2, "Female", "Killing", "Peace"),
     new Animal("Stevie", "Hippo", 2, "Omnivore", "Swamp City", 1, "Male", "Watermelons", "Anything Green"),
   ]
+
+
+
 }
